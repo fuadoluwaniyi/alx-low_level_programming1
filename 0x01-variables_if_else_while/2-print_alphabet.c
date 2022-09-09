09e60ac9f392a3965f9b8c8a24095bd346532434
Fuadoluwaniyi <106689970+fuadoluwaniyi@users.noreply.github.com>
2022-09-09 23:28:52 +0100

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,21 @@
-ssssss
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+char letter; /*Decarling statement*/
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+
+putchar('\n');
+
+return (0);
+
+}
