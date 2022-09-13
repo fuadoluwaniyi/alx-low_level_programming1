8c9e51a55a0bdff77345232bea4de88dda8d800c
Fuadoluwaniyi <106689970+fuadoluwaniyi@users.noreply.github.com>
2022-09-13 18:55:21 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,8 @@
 #include "main.h"
+
 /**
- * main - print Putchar
+ * main - Entry point
+ *
  * Return: Always 0(Success)
  */
 int main(void)
@@ -14,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
