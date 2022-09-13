a64f60ba209f9ac8300de22d67f5a6802be72f59
fuadoluwaniyi <fuadoluwaniyi@gmail.com>
2022-09-13 10:32:14 -0700

    fixing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include"main.h"
+#include "main.h"
 /**
-*main - print Holberton
-*
-*Return: Always 0 (success)
-*/
+ *main - print Holberton
+ *
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
