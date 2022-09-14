07ee556fad2cfcb558a947d07e0a722f59cc5ce4
fuadoluwaniyi <fuadoluwaniyi@gmail.com>
2022-09-14 13:02:03 -0700

    fixing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 	int i;
 	int j;
 
-	if (n <= 15 && >= 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -44,7 +44,7 @@ void print_times_table(int n)
 					_putchar('0' + (prod % 10));
 				}
 			}
-			_putchar(\n);
+			_putchar('\n');
 		}
 	}
 }
