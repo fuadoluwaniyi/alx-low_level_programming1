72f283e94ea8f2d2af69f022a5e14497f0ffc81e
Fuadoluwaniyi <106689970+fuadoluwaniyi@users.noreply.github.com>
2022-09-14 20:24:37 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,50 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_times_table - Print the `n` times table, starting with 0.
- * Description: If `n` is greater than 15 or less than 0, print nothing.
- * @n: int type number
+ * print_times_table - times table function
+ * @n: integer to return
+ *
  */
 void print_times_table(int n)
 {
-	int x = 0, y, z;
+	int i;
+	int j;
 
-	if (n > 15 || n < 0)
-		return;
-	while (x <= n)
+	if (n <= 15 && n >= 0)
 	{
-		for (y = 0; y <= n; y++)
+		for (i = 0; i <= n; i++)
 		{
-			z = x * y;
-			if (z > 99)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(z / 100 + '0');
-				_putchar((z / 10 % 10) + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (z > 9)
-			{
-				_putchar(' ');
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (y != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
-			else
-				_putchar(z + '0');
+				int prod = j * i;
 
-			if (y != n)
-			{
-				_putchar(',');
-				_putchar(' ');
+				if (j == 0)
+				{
+					_putchar('0');
+				} else if (prod <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + prod);
+				} else if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 100));
+					_putchar('0' + ((prod / 10) % 10));
+					_putchar('0' + (prod % 10));
+				} else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' + (prod % 10));
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-		x++;
 	}
 }
-Foot
