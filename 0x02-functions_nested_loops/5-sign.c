ddac7dd3d14b77b37af5d9fc0af52e51f25d0a5b
fuadoluwaniyi <fuadoluwaniyi@gmail.com>
2022-09-14 10:00:39 -0700

    fixing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar('_');
 		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
 	}
 }
