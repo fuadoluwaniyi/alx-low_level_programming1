285cf2eb4ead937bff8d30beaa41dcbe2c1e3e5f
Fuadoluwaniyi <106689970+fuadoluwaniyi@users.noreply.github.com>
2022-09-15 17:23:51 +0100

    Update 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - return  0-9 skips 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
