85fda50a7f75ce4db6865c118109f8a88ca2175a
fuadoluwaniyi <fuadoluwaniyi@gmail.com>
2022-09-15 09:50:24 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - print _ * n
+ * @n: number of _ to print
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
