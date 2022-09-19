491ddff1401a63abaeb083b1178da1bbdecd5f81
Fuadoluwaniyi <106689970+fuadoluwaniyi@users.noreply.github.com>
2022-09-19 07:20:17 +0100

    Update 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - use a pointer to change integer value
+ * @n: parameter
+ * Description - reset_to_98 resets the value of *n to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
